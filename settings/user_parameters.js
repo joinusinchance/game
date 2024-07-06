526e62505e4c17d87bf91de2ba7627bcdb8a03d8
joinusinchance <joinusinchance@gmail.com>
2024-07-06 22:10:47 +0100

    Update user_parameters.js
    added current location and items

--- settings/user_parameters.js
+++ settings/user_parameters.js
@@ -9,15 +9,18 @@ parameters = {
             type = "player"
             , name = "Jack"
             , image = ""
+            , currennt_Location = [0.000,0.0000]
             , cash = 0 -- points or cash??
             , status = 0
-            , won = []
+            , items = []  -- what we have
+            , won = []   -- beaten competetors in game 
             , treasure_Location = [] --[0.0000,0.0000]
-            , current_Person = "unknown"
+            , find_Person = "unknown"
             , discover_location = [] --[0.0000,0.0000]
             ------
             -- cohabitation and boss levels can be managed in score section
             -- score section 
+            , age = 24 -- max 45??
             , wealth = 0 -- max 24?
             , promotions = 0 -- 32?
             , competition = 0 --9?
